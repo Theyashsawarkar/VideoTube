783f7f236e920f748554837b5b1a6a5d6650b399
yash sawarkar <yashsawarkar01@gmail.com>
2024-07-31 19:34:28 +0530

    dot env updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv'
+import 'dotenv/config'
 import connectToDB from './db/dbConnect.js'
 import app from './app.js'
 
-dotenv.config({
-  path: '/env'  // if giving prob try "./.env"
-})
 
 connectToDB()
   .then(() => {
